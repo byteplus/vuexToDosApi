6dcec0bb19e373478935b4b1affad7c0e2120c4a
Angel <angeldimov777@gmail.com>
2020-03-25 19:54:43 +0000

    done

---
+++ src/components/FilterTodos.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        Filter Dotos
+        <select @change="filterTodos($event)">
+            <option value="all">All</option>
+            <option value="100">100</option>
+            <option value="50">50</option>
+            <option value="20">20</option>
+            <option value="12">12</option>
+        </select>
+    </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+    export default {
+    name: "FilterTodos",
+    methods: mapActions(["filterTodos"])
+    };
+</script>
+
+<style scoped>
+select {
+  margin-top: 20px;
+  padding: 6px;
+  border: #41b883 1px solid;
+}
+</style>
